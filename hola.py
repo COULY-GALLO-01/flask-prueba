31c7013cd2c35f63f6e2cd4c00fd6e13ac7541c9
PATRICIO GALLO DILLON <d47351717@alumnos.itr.edu.ar>
2024-05-14 14:25:10 -0300

    si

--- hola.py
+++ hola.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app=Flask(__name__)
 
+
+@app.route('/')
+def principal():
+    
+    return render_template('index.html')#aca poner link a archivos front end, como la ruta esta en "/" es lo primero que vemos, todo en la carpeta templates, asi es el programa
+
+@app.route('/test')
+def secondary():
+  
+    return render_template('test.html')
+
+
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True, port=5000)
+
