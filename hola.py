6ef898a9f166114d1e195e77924e032ab4c17cf3
PATRICIO GALLO DILLON <d47351717@alumnos.itr.edu.ar>
2024-05-17 08:49:52 -0300

    arreglar el padre

--- hola.py
+++ hola.py
@@ -7,12 +7,11 @@ app=Flask(__name__)
 @app.route('/')
 def principal():
     
-    return render_template('index.html')#aca poner link a archivos front end, como la ruta esta en "/" es lo primero que vemos, todo en la carpeta templates, asi es el programa
-
-@app.route('/test')
-def secondary():
-  
-    return render_template('test.html')
+    return render_template('test.html')#aca poner link a archivos front end, como la ruta esta en "/" es lo primero que vemos, todo en la carpeta templates, asi es el programa
+@app.route('/son')
+def son():
+    
+    return render_template('son.html')#aca poner link a archivos front end, como la ruta esta en "/" es lo primero que vemos, todo en la carpeta templates, asi es el programa
 
 
 if __name__=='__main__':
